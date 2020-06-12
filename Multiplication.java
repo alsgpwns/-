40852c8895f7e882fb85c6d1aa56ff178bf837da
Min Dal-Pong <45223821+alsgpwns@users.noreply.github.com>
2020-06-12 17:43:38 +0900

    Multiplication table
    :D

---
+++ Multiplication.java
@@ -0,0 +1,19 @@
+class Multiplication{
+	public static void main(String[] args){
+		int i, j, result;
+
+		for(i=2;i<=9;i++)
+		{
+			//System.out.println(---------);
+			for(j=1; j<=9; j++)
+			{
+			result=i*j;
+			System.out.print(i+"*"+j+"="+result+"\n");
+			}
+			System.out.println("---------");
+		}
+
+		
+
+	}
+}
